65dac2378b529d84eff4a738a42a23503a4fde86
Lydia <lydiaprentice@hotmail.co.uk>
2023-07-07 20:03:04 +0100

    Integrate the API with the forecast

--- src/app.js
+++ src/app.js
@@ -1,26 +1,35 @@
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let weatherForecast = response.data.daily;
   let forecast = document.querySelector("#weather-forecast");
   let forecastHTML = `<div class="row align-items-end">`;
-
-  let days = ["Thur", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  weatherForecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
           <div class="col">
-            <strong>${day}</strong>
+            <strong>${formatDay(forecastDay.dt)}</strong>
             <br />
-            <span class="max-temp">20°C</span>
-            <span class="min-temp">15°C</span>
+            <span class="max-temp">${Math.round(forecastDay.temp.max)}°C</span>
+            <span class="min-temp">${Math.round(forecastDay.temp.min)}°C</span>
             <br />
             <img
-              src="https://openweathermap.org/img/wn/10d@2x.png"
+              src="https://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
               alt=""
               width="90"
             />
           </div>
         `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecast.innerHTML = forecastHTML;
@@ -28,7 +37,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "7784a4cd4aa2e0c25ead7bd96d585b8a";
-  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric}`;
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(url).then(displayForecast);
 }
 
